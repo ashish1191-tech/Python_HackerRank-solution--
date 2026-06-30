4516a217f7f95428f0d86a741dfcc8a792c64f65
ashish1191-tech <ashishsts11s@gmail.com>
2026-06-30 21:26:14 +0530

    Create 017_Mutations.py

---
+++ 017_Mutations.py
@@ -0,0 +1,8 @@
+def mutate_string(string, position, character):
+    return string[:position] + character + string[position + 1:]
+
+if __name__ == '__main__':
+    s = input()
+    i, c = input().split()
+    result = mutate_string(s, int(i), c)
+    print(result)
